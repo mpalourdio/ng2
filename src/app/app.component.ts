05ad0b74202d84d500f08b06e76c204a1f9c8dbc
Michel Palourdio <mpalourdio@gmail.com>
2017-03-26 15:46:19 +0200

    Quick hacks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
+import {Dummy} from "./dummy";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+    title = 'app works!';
+    inputText: Dummy;
+
+
+    ngOnInit(): void {
+        this.after3seconds();
+    }
+
+    after3seconds(): void {
+        let counter = 1;
+        this.inputText = new Dummy();
+        setInterval(() => {
+            this.inputText.id = counter;
+            counter++;
+        }, 1000);
+    };
 }
---
+++ src/app/second/second.component.ts
@@ -0,0 +1,15 @@
+import {Component, Input} from "@angular/core";
+
+@Component({
+    selector: 'app-second',
+    templateUrl: './second.component.html',
+    styleUrls: ['./second.component.css']
+})
+export class SecondComponent {
+    @Input()
+    bindingFromAppComponent;
+
+    constructor() {
+    }
+
+}
