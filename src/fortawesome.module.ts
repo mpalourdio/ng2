30223b69464070112634dfb3775effc53d2b3441
Michel Palourdio <mpalourdio@gmail.com>
2018-03-22 10:52:37 +0100

    import exported default

--- src/fortawesome.module.ts
+++ src/fortawesome.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import fontawesome from '@fortawesome/fontawesome';
-import faAddressBook from '@fortawesome/fontawesome-free-regular';
+import packr from '@fortawesome/fontawesome-free-regular';
+import packs from '@fortawesome/fontawesome-free-solid';
 
 @NgModule()
 export class FortAwesomeModule {
     constructor() {
-        fontawesome.library.add(faAddressBook);
+        fontawesome.library.add(packr);
+        fontawesome.library.add(packs);
     }
 }
