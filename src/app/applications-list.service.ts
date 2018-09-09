865dfaa96e94e453979163345a3e049670656677
Michel Palourdio <mpalourdio@gmail.com>
2018-09-09 19:24:33 +0200

    Specify methods visibility

--- src/app/applications-list.service.ts
+++ src/app/applications-list.service.ts
@@ -7,11 +7,11 @@ import { mapTo, switchMap } from 'rxjs/operators';
 })
 export class ApplicationsListService {
 
-    delay(delayMs) {
+    private delay(delayMs) {
         return source => source.pipe(switchMap(value => timer(delayMs).pipe(mapTo(value))));
     }
 
-    applicationsList(): Observable<string[]> {
+    public applicationsList(): Observable<string[]> {
         return of([
             {name: 'netbeans', isFav: false},
             {name: 'mega', isFav: false},
