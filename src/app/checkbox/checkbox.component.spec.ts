69ba20443f8bf30d9bac1090a8670bece471856b
Michel Palourdio <mpalourdio@gmail.com>
2018-11-16 21:15:20 +0100

    Cleanup imports

--- src/app/checkbox/checkbox.component.spec.ts
+++ src/app/checkbox/checkbox.component.spec.ts
@@ -2,7 +2,6 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CheckboxComponent } from './checkbox.component';
 import { By } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { DebugElement } from '@angular/core';
 
 describe('CheckboxComponent', () => {
     let component: CheckboxComponent;
