d7d7e053dae237dbe05f3668b19475be907b5d16
Michel Palourdio <mpalourdio@gmail.com>
2018-12-08 19:42:15 +0100

    Improve test suite

--- src/app/intl-tel-input/intl-tel-input.component.spec.ts
+++ src/app/intl-tel-input/intl-tel-input.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, NgForm } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 import { IntlTelInputComponent } from './intl-tel-input.component';
 
 describe('IntlTelInputComponent', () => {
@@ -31,14 +32,14 @@ describe('IntlTelInputComponent', () => {
         expect(component).toBeTruthy();
     });
 
-    it('should convert phone number to E164 format', async(() => {
+    it('should convert phone number to E164 format', () => {
         component.countryCodes = ['ch', 'fr'];
         component.phoneNumber = '0797703808';
 
         expect(component.E164PhoneNumber).toBe('+41797703808');
-    }));
+    });
 
-    it('should try to re-set E164 phone number on countryChange', async(() => {
+    it('should try to re-set E164 phone number on countryChange', () => {
         component.countryCodes = ['ch', 'fr'];
         component.phoneNumber = '0797703808';
 
@@ -48,5 +49,62 @@ describe('IntlTelInputComponent', () => {
         component.intlTelInput.setCountry('fr');
 
         expect(component.E164PhoneNumber).toBe('+33681215656');
-    }));
+    });
+
+    it('should add a label tag if label attribute is set', () => {
+        const labelText = 'label text';
+        component.label = labelText;
+        fixture.detectChanges();
+
+        const element = fixture
+            .debugElement
+            .query(By.css('label'))
+            .nativeElement;
+
+        expect(element).not.toBe(null);
+        expect(element.innerText).toBe(labelText);
+    });
+
+    it('should not add a label by default', () => {
+        const element = fixture
+            .debugElement
+            .query(By.css('label'));
+
+        expect(element).toBe(null);
+    });
+
+    it('should set both required and aria-required if specified', () => {
+        component.required = true;
+        fixture.detectChanges();
+
+        const element: HTMLElement = fixture
+            .debugElement
+            .query(By.css('input'))
+            .nativeElement;
+
+        expect(element.getAttribute('required')).toBe('required');
+        expect(element.getAttribute('aria-required')).toBe('true');
+    });
+
+    it('should set name and id to the same value', () => {
+        const element: HTMLElement = fixture
+            .debugElement
+            .query(By.css('input'))
+            .nativeElement;
+
+        expect(element.getAttribute('name')).toBe(element.getAttribute('id'));
+    });
+
+    it('should allow specifying a css class', () => {
+        const cssClass = 'my-css-class';
+        component.cssClass = cssClass;
+        fixture.detectChanges();
+
+        const element: HTMLElement = fixture
+            .debugElement
+            .query(By.css('input'))
+            .nativeElement;
+
+        expect(element.className).toContain(cssClass);
+    });
 });
