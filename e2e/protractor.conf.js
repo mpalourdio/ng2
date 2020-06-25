d86bda9f77fc4fc472e9ababf89e08044869fc18
Michel Palourdio <mpalourdio@gmail.com>
2020-06-25 12:28:58 +0200

    chore(ng10): Fix protractor conf

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -2,7 +2,7 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-const { SpecReporter } = require('jasmine-spec-reporter');
+const {SpecReporter, StacktraceOption} = require('jasmine-spec-reporter');
 const chromeExecutablePath = require('puppeteer').executablePath();
 
 /**
@@ -45,7 +45,8 @@ exports.config = {
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
-    print: function() {}
+    print: function () {
+    }
   },
   onPrepare() {
     require('ts-node').register({
